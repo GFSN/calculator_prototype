0f78e07c4ea4cd9eb678f355ddf87dcd24dcb52d
GFSN <nilisnw@gmail.com>
2017-10-06 06:28:29 +0500

    width_test
    text

--- app.coffee
+++ app.coffee
@@ -196,46 +196,8 @@ textLayer.backgroundColor = "rgba(0, 0, 0, 1)"
 
 # textLayer.input.addEventListener "keyup", ->
 
-class InputTextLayer extends Framer.Layer
-
-	constructor: (options) ->
-
-		super options
-
-		@ignoreEvents = false
-		@input = document.createElement("input")
-
-		_.extend @input.style,
-			"-webkit-user-select": "text"
-			"-webkit-box-sizing": "border-box"
-			"cursor": "auto"
-		@_update()
-		@_element.appendChild(@input)
-
-		@on "change:width", @_update
-		@on "change:height", @_update
-
-	_update: =>
-		_.extend @input.style,
-			width: "#{@width}px"
-			height: "#{@height}px"
-
-
-textLayer = new InputTextLayer width:Screen.width , height:120, y:0, x:0
-textLayer.states.animationOptions = curve: "spring(250, 20, 0)"
-
-textLayer.input.style.font = "36px/1.35em Helvetica"
-textLayer.input.style.font-style = "normal"
-textLayer.input.style.font-weight = "100"
-textLayer.input.style.padding = "24px"
-textLayer.input.style.textAlign = "left"
-textLayer.input.style.background = "transparent"
-textLayer.input.style.outline = '0'
-textLayer.input.style.color = "#fff"
-textLayer.input.style.border = "0px solid"
-# textLayer.input.style.display = "none"
-textLayer.input.value = new Date().getSeconds()
-textLayer.input.placeholder = "Search Me"
-textLayer.backgroundColor = "rgba(0, 0, 0, 1)"
-
-# textLayer.input.addEventListener "keyup", ->
+layerWidth = new Layer
+  x: 0
+  y: 250
+  width: 740
+  height: 250
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var InputTextLayer, layerA, layerB, layerB1, layerB2, layerB3, style, textLayer, weight,
+var InputTextLayer, layerA, layerB, layerB1, layerB2, layerB3, layerWidth, style, textLayer, weight,
   indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; },
   bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
@@ -278,67 +278,9 @@ textLayer.input.placeholder = "Search Me";
 
 textLayer.backgroundColor = "rgba(0, 0, 0, 1)";
 
-InputTextLayer = (function(superClass) {
-  extend(InputTextLayer, superClass);
-
-  function InputTextLayer(options) {
-    this._update = bind(this._update, this);
-    InputTextLayer.__super__.constructor.call(this, options);
-    this.ignoreEvents = false;
-    this.input = document.createElement("input");
-    _.extend(this.input.style, {
-      "-webkit-user-select": "text",
-      "-webkit-box-sizing": "border-box",
-      "cursor": "auto"
-    });
-    this._update();
-    this._element.appendChild(this.input);
-    this.on("change:width", this._update);
-    this.on("change:height", this._update);
-  }
-
-  InputTextLayer.prototype._update = function() {
-    return _.extend(this.input.style, {
-      width: this.width + "px",
-      height: this.height + "px"
-    });
-  };
-
-  return InputTextLayer;
-
-})(Framer.Layer);
-
-textLayer = new InputTextLayer({
-  width: Screen.width,
-  height: 120,
-  y: 0,
-  x: 0
+layerWidth = new Layer({
+  x: 0,
+  y: 250,
+  width: 740,
+  height: 250
 });
-
-textLayer.states.animationOptions = {
-  curve: "spring(250, 20, 0)"
-};
-
-textLayer.input.style.font = "36px/1.35em Helvetica";
-
-textLayer.input.style.font - (style = "normal");
-
-textLayer.input.style.font - (weight = "100");
-
-textLayer.input.style.padding = "24px";
-
-textLayer.input.style.textAlign = "left";
-
-textLayer.input.style.background = "transparent";
-
-textLayer.input.style.outline = '0';
-
-textLayer.input.style.color = "#fff";
-
-textLayer.input.style.border = "0px solid";
-
-textLayer.input.value = new Date().getSeconds();
-
-textLayer.input.placeholder = "Search Me";
-
-textLayer.backgroundColor = "rgba(0, 0, 0, 1)";
